e64c6b5a54e499fc487879627737abe643c1f087
Stefanlars <84471873+Stefanlars@users.noreply.github.com>
2022-05-04 13:10:25 -0500

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -34,7 +34,19 @@ class LinkedList:
         string_list = string_list + str(current_node.get_value()) + '\n'
       current_node = current_node.get_next_node()
     return string_list
-        
+  
+  def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node        
     
 
 
